851d26145887d28b94acd822f0c3ddb09fbc939a
Raolis <raolis1989@gmail.com>
2019-12-28 01:30:04 -0300

    routes

--- digitalsales/src/App.vue
+++ digitalsales/src/App.vue
@@ -25,7 +25,7 @@
                       </v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
-                <v-list-item :to="{name:''}">
+                <v-list-item :to="{name:'categories'}">
                     <v-list-item-action>
                       <v-icon>table_chart</v-icon>
                     </v-list-item-action>
